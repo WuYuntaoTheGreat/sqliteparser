e4a59c1897b4c666fb0e33338723ce2307384697
Wu Yuntao <wuyuntao@gmail.com>
2015-09-01 16:41:31 +0800

    Add index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+/*!
+ * sqliteparser Copyright(c) 2015 Wu Yuntao
+ * https://github.com/WuYuntaoTheGreat/sqliteparser.git
+ * Released under the MIT License.
+ */
+module.exports = require('./src/parser');
+
